735c79abe511849e335fa6b24ebf9c2b882d779a
Anton-Abramovich_wkl <anton.abramovich@wolterskluwer.com>
2026-03-10 14:25:52 +0100

    fix search initial loading state

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -5,9 +5,7 @@ export default function SearchLoadingPage() {
   return (
     <div className={'flex flex-col gap-6 mt-6'}>
       <FiltersSkeleton />
-      <div className={'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'}>
-        <SearchResultsListSkeleton />
-      </div>
+      <SearchResultsListSkeleton />
     </div>
   );
 }
