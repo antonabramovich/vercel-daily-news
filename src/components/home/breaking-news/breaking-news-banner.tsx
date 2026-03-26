73122b9678e144804106470eac235e4db55fc3a9
Anton Abramovich <aabramovich911@gmail.com>
2026-03-26 17:14:07 +0100

    remove extra breaking news markup

--- src/components/home/breaking-news/breaking-news-banner.tsx
+++ src/components/home/breaking-news/breaking-news-banner.tsx
@@ -15,8 +15,8 @@ export async function BreakingNewsBanner() {
   return (
     <div className={'absolute top-[65px] left-0 right-0 py-2 px-4 bg-foreground text-background border-b'}>
       <div className={'mx-auto max-w-4xl flex gap-4 items-center whitespace-nowrap'}>
-        <span><TriangleAlert /></span>{' '}
-        <Badge>Breaking</Badge>{' '}
+        <TriangleAlert />
+        <Badge>Breaking</Badge>
         <Link href={`/articles/${slug}`} className={'hover:underline overflow-hidden text-ellipsis'}>{headline}</Link>
       </div>
     </div>
