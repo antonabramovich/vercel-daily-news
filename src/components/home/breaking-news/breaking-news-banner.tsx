d819ddfcdca4009cf4b6f645fce84f1cf5170bbc
Anton Abramovich <aabramovich911@gmail.com>
2026-04-13 08:44:15 +0200

    tweak breaking news link underline style a bit

--- src/components/home/breaking-news/breaking-news-banner.tsx
+++ src/components/home/breaking-news/breaking-news-banner.tsx
@@ -15,10 +15,10 @@ export async function BreakingNewsBanner() {
 
   return (
     <div className={'absolute top-[65px] left-0 right-0 py-2 px-4 bg-foreground text-background border-b'}>
-      <Link href={`/articles/${slug}`} className={'mx-auto max-w-4xl flex gap-4 items-center whitespace-nowrap hover:underline'}>
+      <Link href={`/articles/${slug}`} className={'mx-auto max-w-4xl flex gap-4 items-center whitespace-nowrap group'}>
         <TriangleAlert className={'shrink-0 size-4 xs:size-6'} />
         <Badge>Breaking</Badge>
-        <span className={'overflow-hidden text-ellipsis'}>{headline}</span>
+        <span className={'overflow-hidden text-ellipsis group-hover:underline'}>{headline}</span>
       </Link>
     </div>
   );
